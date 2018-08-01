a1d30d554b5bedcb11f4ef49395a5edbf7be83d2
Dhaval Patel <dhaval96patel@gmail.com>
2018-07-31 22:08:25 -0500

    In the Ravenous Part 4 updated App and Yelp file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,15 +10,15 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      business: [];
+      businesses: []
     };
-    this.searchYelp.bind(this);
+    this.searchYelp = this.searchYelp.bind(this);
   }
 
   searchYelp(term, location, sortBy){
-    Yelp.search(term,location, sortBy).then(business => {
-      return setState({
-        businesses: businesses;
+    Yelp.search(term,location, sortBy).then(businesses => {
+      this.setState({
+        businesses: businesses
       });
     });
   }
@@ -28,7 +28,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp = {this.searchYelp}/>
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
