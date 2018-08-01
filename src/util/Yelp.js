4329a0678cf84201585b74204b6542aa1959ad1a
Dhaval Patel <dhaval96patel@gmail.com>
2018-07-31 19:15:55 -0500

    Ravenous Part 4 of the app connecting the Yelp API

---
+++ src/util/Yelp.js
@@ -0,0 +1,31 @@
+const apiKey = '6WS2EkXk9P7eSEx1T50Kcx8LrL6U0sAP5OeDxoofPpvOvFMbWuuRVSmlvR3MKIauQdJJ-tgpipENR_e0Q7rU2fct93eZGCOfnLGeTGq2Xor4vQjdWcvjUdxQrpZgW3Yx';
+
+const Yelp  = {
+  const search(term, location, sortBy){
+   return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+    {
+      headers: {
+        {Authorization: `Bearer ${apiKey}`}
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if(jsonResponse.businesses){
+      return  jsonResponse.businesses.map(business => {
+            id: business.id,
+            imageSrc: business.image_url;
+            name: business.name;
+            address: business.location.address1;
+            city: business.location.city;
+            zipCode: business.location.zip_code;
+            category: business.category[0].title;
+            rating: business.rating;
+            reviewCount: business.review_count;
+
+        });
+      }
+    });
+  }
+};
+
+export default Yelp;
